2d5669680c8a2b6d1ce6cfee23a7a8b45906c181
svang56 <svang56@mail.csuchico.edu>
2018-04-11 17:43:21 -0700

    Done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include<cstdlib>
+#include <ctime>
+
 
 using namespace std;
 
@@ -8,8 +11,13 @@ const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
 
+void printScoreboard(vector< vector<int> >);
+int randomBetween(int first, int second);
+
 int main()
 {
+srand((int) time(0));
+
   int periods;
   int teams;
   vector< vector<int> > scoreBoard;
@@ -39,28 +47,59 @@ int main()
 
   for(int row = 0; row < scoreBoard.size(); row++)
   {
-    for(int col = 0; col < scoreBoard.size(); col++)
+    for(int col = 0; col < scoreBoard[row].size(); col++)
   {
     scoreBoard[row][col] = 0;
   }
 
   }
 
- cout<<"SCOREBOARD"<<endl;    
- for(int row = 0; row < scoreBoard.size(); row++)
-  {
-    cout<<"Player "<<row + 1<<": ";
-    for( int col = 0; col < scoreBoard[row].size(); col++)
-    {
-    cout<<scoreBoard[row][col]<<"|";
-    }
-    cout<<endl;
-  }
 
-  
+  printScoreboard(scoreBoard);
+
+  for(int r = 0; r < scoreBoard.size(); r++)
+  {
+    for(int c = 0; c < scoreBoard[r].size(); c++)
+    {   
+        scoreBoard[r][c] = randomBetween(5,5);
+
+    }
+  }  
+printScoreboard(scoreBoard);
    //make scoreboard and fill it with zeros
   
    //once created, display the scoreboard
   }
   return 0;
+}
+
+void printScoreboard(vector< vector<int> > grid)
+{
+    cout<<"SCOREBOARD\n";
+
+ for(int row = 0; row < grid.size(); row++)
+  {
+    cout<<"Player "<<row + 1<<": ";
+    for( int col = 0; col < grid[row].size(); col++)
+    {
+    cout<<grid[row][col]<<"|";
+    }
+    cout<<endl;
+  }
+
+}
+int randomBetween(int first, int second)
+{
+
+    if(first > second)
+    {
+       return  second + rand()%(first - second +1);
+    }
+    else
+    {
+    return first + rand()%(second - first +1);
+    }
+
+
+
 }
